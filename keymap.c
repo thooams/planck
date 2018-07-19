9d1594f24fb95795476694dde1101ddb770aafaa
Thomas HUMMEL <thomas@hummel.link>
2018-07-19 14:17:59 +0200

    [...work in progress...]

--- keymap.c
+++ keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
   {KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {RAISE,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {LOWER,   KC_LCTL, KC_LALT, KC_LGUI, KC_TAB,  KC_SPC,  KC_SPC,  KC_BSPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {LOWER,   KC_LCTL, KC_LGUI, KC_RALT, KC_TAB,  KC_SPC,  KC_SPC,  KC_BSPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* AZERTY
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  FR_A,    FR_Z,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC},
   {KC_LSFT, KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_M,     KC_QUOT},
   {RAISE,   KC_W,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    FR_COMM, FR_SCLN, FR_COLN, KC_EQUAL, KC_ENT },
-  {LOWER,   KC_LCTL, KC_LALT, KC_LGUI, KC_TAB,  KC_SPC,  KC_SPC,  KC_BSPC,   KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT}
+  {LOWER,   KC_LCTL, KC_LGUI, KC_RALT, KC_TAB,  KC_SPC,  KC_SPC,  KC_BSPC,   KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT}
 },
 
 /* Bépo
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  BP_B,       BP_E_ACUTE, BP_P,    BP_O,    BP_E_GRAVE, BP_DEAD_CIRCUMFLEX, BP_V,    BP_D,    BP_L,    BP_J,  BP_W},
   {KC_LSFT, BP_A,       BP_U,       BP_I,    BP_E,    BP_COMMA,   BP_C,               BP_T,    BP_S,    BP_R,    BP_N,  BP_M},
   {RAISE,   BP_A_GRAVE, BP_Y,       BP_X,    BP_DOT,  BP_K,       BP_APOS,            BP_Q,    BP_G,    BP_H,    BP_F,  BP_Z},
-  {LOWER,   KC_LCTL,    KC_LALT,    KC_LGUI, KC_SPC , KC_TAB,      KC_BSPC,           KC_ENT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
+  {LOWER,   KC_LCTL,    KC_LGUI,    BP_ALGR, KC_SPC , KC_TAB,      KC_BSPC,           KC_ENT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT}
 },
 
 /* Lower
@@ -111,8 +111,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = {
   {_______, _______, _______,     _______,      _______,     KC_MPRV,     KC_MPLY,      KC_MNXT,     KC__MUTE, KC_VOLD, KC_VOLU,  KC_BSPC},
   {_______, _______, _______,     _______,      _______,     _______,     BP_C_CEDILLA, KC_LEFT,     KC_DOWN,  KC_UP,   KC_RIGHT, KC_ENT},
-  {_______, _______, CTL_T(BP_W), CTL_T(BP_X),  CTL_T(BP_C), CTL_T(BP_P), CTL_T(BP_T),  CTL_T(BP_Z), _______,  _______, _______,  _______},
-  {_______, _______, _______,     CTL_T(KC_F2), _______,     _______,     KC_BSPC,      KC_ENT,      _______,  _______, _______,  _______}
+  {_______, _______, LCTL(BP_W), LCTL(BP_X),  LCTL(BP_C), LCTL(BP_P), LCTL(BP_T),  LCTL(BP_Z), _______,  _______, _______,  _______},
+  {_______, _______, _______,     LCTL(KC_F2), _______,     _______,     KC_BSPC,      KC_ENT,      _______,  _______, _______,  _______}
 },
 
 /* Raise
@@ -137,18 +137,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|      |      |Qwerty|Azerty| Bépo |      |      |
+ * |      |      |      |      |      |      |      |Qwerty|Azerty| Bépo |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |Mus on|Musoff|      |      |Term n|Term f|      |      |      |
+ * |      |      |      |Mus on|Musoff|      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |chroma|Guitar|Violin|Major |
+ * |      |      |      |      |      |             |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   DEBUG,    _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL },
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  _______, _______, QWERTY,  AZERTY, BEPO,  _______,  _______},
-  {_______, _______,  _______,  MU_ON,   _______,  _______,   _______,  TERM_ON, TERM_OFF, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, DISNEY}
+  {_______, RESET,   _______,    _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL },
+  {_______, _______, _______,  _______,   _______,  _______, _______, BEPO,  AZERTY, QWERTY,  _______,  _______},
+  {_______, _______,  _______,  MU_ON,   MU_OFF,  _______,   _______,  _______, _______, _______, _______, _______},
+  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
 }
 
 
